8faff454959ec63a937cf302b83924bf00e5ce9f
Piotr Wesolowski <kontakt@piotrwesolowski.pl>
2023-01-24 21:14:02 +0100

    Create IsStringPalindrome.swift
    Function for checking if string is palindrome has been added.

---
+++ IsStringPalindrome.swift
@@ -0,0 +1,16 @@
+//
+//  IsStringPalindrome.swift
+//  
+//
+//  Created by Piotr Wesołowski on 24/01/2023.
+//
+
+import Foundation
+
+func isStringPalindrome(input: String) -> Bool {
+    
+}
+
+assert(isStringPalindrome(input: "rotator") == true, "String isn't palindrome.")
+
+assert(isStringPalindrome(input: "Hello, world") == true, "String isn't palindrome.")
